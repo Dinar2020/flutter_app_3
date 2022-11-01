bd9de3f5283947133a23e1c4080d428e60e0855e
dinario <dinario123@gmail.com>
2022-11-01 18:45:06 -0300

    adicionando nivel e linnha de progesso para ao clicar o no botão pra cima

--- lib/main.dart
+++ lib/main.dart
@@ -7,71 +7,106 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text ('Flutter: Curso de Flutter'),
+          title: const Text('Flutter: Curso de Flutter'),
         ),
         body: ListView(
-          children: const [ Task('iniciando o curso'),
-          Task('fazendo o curso'),
-          Task('Finalizando o curso'),
-          Task('iniciando o curso'),
-          Task('fazendo o curso'),
-          Task('Finalizando o curso'),
-
-        ],
+          children: [
+            Task('iniciando o curso'),
+            Task('fazendo o curso'),
+            Task('Finalizando o curso'),
+            Task('iniciando o curso'),
+            Task('fazendo o curso'),
+            Task('Finalizando o curso'),
+          ],
         ),
-        floatingActionButton: FloatingActionButton( onPressed:  () { const Task('Finalizando o curso');
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          Task('Finalizando o curso');
         }),
-        ),
-      );
+      ),
+    );
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String frase;
   const Task(this.frase, {super.key});
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int level = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child:Column (children:<Widget> [ 
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
                     ),
                     Text(
-                      frase,
+                      widget.frase,
                       style: const TextStyle(
-                        fontSize: 24,overflow: TextOverflow.ellipsis
-                      ),
+                          fontSize: 24, overflow: TextOverflow.ellipsis),
                     ),
-                    ElevatedButton(onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-                ]),
-              ),
-              Container(
-                color: Colors.blue,
-                height: 50,
-              ),
-      ],
-      )
-    );
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            print(level++);
+                          });
+                        },
+                        child: const Icon(Icons.arrow_drop_up))
+                  ]),
+            ),
+            Container(
+              color: Colors.blue,
+              height: 50,
+              width: 500,
+              child:Center(
+                child:Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [ Container(
+                      child: LinearProgressIndicator(color: Colors.white,value: level / 10),
+                      width: 200,),
+                      
+                      
+                      
+                      Text(textAlign: TextAlign.center,
+
+                      'Nível: $level',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        
+                      ),),
+                    ],
+                  ),
+                ),
+                ),
+              
+            ),
+          ],
+        ));
   }
 }
